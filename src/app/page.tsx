d744c1b7433d96be463cd23063e66614dc75c62c
Ofek Avidan <ofekavidan5@gmail.com>
2025-06-14 21:21:03 +0300

    feat: update page metadata (title and description)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
 import Image from "next/image";
-// import {
-//   Tabs,
-//   TabsContent,
-//   TabsList,
-//   TabsTrigger,
-// } from "C:/Users/DELL/projects/brain-visualization/components/ui/tabs";
-// import {
-//   Card,
-//   CardContent,
-// } from "C:/Users/DELL/projects/brain-visualization/components/ui/card";
+
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Card, CardContent } from "@/components/ui/card";
 
+export const metadata = {
+  title: "Klotho KO Brain Visualization",
+  description: "Interactive viewer for microRNA and tRNA brain data",
+};
 
 export default function Home() {
   return (
